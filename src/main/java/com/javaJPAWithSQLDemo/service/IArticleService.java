f9c0abf0d6ca56b42a3b44627490b130d4597088
Surbhi Dhawan <surbhi.dhawan@tothenew.com>
2018-08-05 21:44:08 +0530

    Demo for JPA with SQL and REST

---
+++ src/main/java/com/javaJPAWithSQLDemo/service/IArticleService.java
@@ -0,0 +1,13 @@
+package com.javaJPAWithSQLDemo.service;
+
+import java.util.List;
+
+import com.javaJPAWithSQLDemo.entity.Article;
+
+public interface IArticleService {
+     List<Article> getAllArticles();
+     Article getArticleById(int articleId);
+     boolean addArticle(Article article);
+     void updateArticle(Article article);
+     void deleteArticle(int articleId);
+}
